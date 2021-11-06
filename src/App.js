ed1207859881e089ca4b33eddd3482876f763302
Rohan Patel <rohanbpatel98@gmail.com>
2021-11-06 01:30:01 -0500

    adding updated app version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Chat from "./chat/chat";
+import Process from "./process/process";
+import Home from "./home/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import React from "react";
+import io from "socket.io-client";
 
+const socket = io.connect('/');
+function Appmain(props) {
+  return (
+    <React.Fragment>
+      <div className="right">
+        <Chat
+          username={props.match.params.username}
+          roomname={props.match.params.roomname}
+          socket={socket}
+        />
+      </div>
+      <div className="left">
+        <Process />
+      </div>
+    </React.Fragment>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home socket={socket} />
+          </Route>
+          <Route path="/chat/:roomname/:username" component={Appmain} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
