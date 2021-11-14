6391c3c62014c05e16a527a5a494f934591d8d92
Gabriel Chen <gabriel.luke.chen@gmail.com>
2021-11-13 22:19:44 -0800

    add amplify

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import Chat from "./chat/chat";
 import Home from "./home/home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 
 const socket = io.connect('http://localhost:8000');
 function Appmain(props) {
@@ -18,17 +20,43 @@ function Appmain(props) {
   );
 }
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact>
-            <Home socket={socket} />
-          </Route>
-          <Route path="/chat/:roomname/:username" component={Appmain} />
-        </Switch>
-      </div>
-    </Router>
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData)
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <AmplifySignOut />
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact>
+              <Home socket={socket} />
+            </Route>
+            <Route path="/chat/:roomname/:username" component={Appmain} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+          { type: "family_name", label: "Last Name *", required: true},
+          { type: "given_name", label: "First Name *", required: true},
+        ]}
+      />
+    </AmplifyAuthenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import React from "react";
 import { Provider } from "react-redux";
+import Amplify from 'aws-amplify';
+import * as Config from './config';
+
+Amplify.configure({
+  Auth: {
+    region: Config.AMPLIFY_AUTH_REGION,
+    userPoolId: Config.AMPLIFY_AUTH_USER_POOL_ID,
+    userPoolWebClientId: Config.AMPLIFY_AUTH_USER_POOL_CLIENT_ID,
+    userPoolWebClientSecret: Config.AMPLIFY_AUTH_USER_POOL_CLIENT_SECRET,
+  }
+});
 
 
 const store = createStore(rootReducers);
