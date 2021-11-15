fae49b4083c005b25883b0ffb1358279d46182c7
Soham Bhattacharjee <soham.onlyn@gmail.com>
2021-11-14 16:43:36 -0800

    null check

--- src/home/home.js
+++ src/home/home.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import { get } from 'lodash'
 import "./home.scss";
 import { Link } from "react-router-dom";
 
 function Homepage(props) {
   const [roomname, setroomname] = useState("");
-  
+
   return (
     <div className="homepage">
-      <h1>MooChat - {`${props.user.attributes.given_name}`}</h1>
+      <h1>MooChat - {get(props, `user.attributes.given_name`)}</h1>
       <input
         placeholder="Input the room name"
         value={roomname}
